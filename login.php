5251c8440d8c44c7cccd4b7f52c2bdf13441de6c
Amith <akoligudde@autumnworldwide.com>
2017-06-19 16:18:55 +0530

    initial commit-db connectivity and login form created

---
+++ login.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>login</title>
+        <link rel="stylesheet" type="text/css" href="style.css" >
+    </head>
+
+    <body>
+        <div id="frm">
+            <form action="process.php" method="post">
+                <p>
+                    <label>Username:</label>
+                    <input type="text" id="user" name="user" />
+                </p>
+                <p>
+                    <label>Password:</label>
+                    <input type="password" id="pass" name="pass" />
+                </p>
+                <p>
+
+                    <input type="submit" id="btn" value="login" />
+                </p>
+
+
+
+            </form>
+
+    </body>
+</html>
---
+++ process.php
@@ -0,0 +1,23 @@
+<?php
+
+$username = $_POST['user'];
+$password = $_POST['pass'];
+
+#connect to database
+
+$conn = mysql_connect("localhost", "root", "");
+$db = mysql_select_db("login");
+
+$sql="INSERT INTO user(username,password) values('$username','$password')";
+
+$result = mysql_query("select * from user where username='$username' and password='$password'")
+        or die("Failed to query database" . mysql_error());
+
+$row = mysql_fetch_array($result);
+
+if ($row['username'] == $username && $row['password'] == $password) {
+    echo "Login Sucessful !!! WelCome" . $row['username'];
+} else {
+    echo "Failed to Login....";
+}
+?>          
